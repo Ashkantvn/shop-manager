73ec3b15ab8dda52d3decbfc50ba630e04000cdd
Ashkan <ashkantavangar7@gmail.com>
2025-05-17 13:36:29 +0330

    Iinitialize accounts

--- core/core/settings.py
+++ core/core/settings.py
@@ -40,6 +40,7 @@ CELERY_TASK_SERIALIZER = 'json'
 INSTALLED_APPS = [
     'daphne',
     'channels',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
