e1194c2554497c5191bc88bb7da12845f32e5980
Ashkan <ashkantavangar7@gmail.com>
2025-05-27 19:29:55 +0330

    Update urls of account

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -5,7 +5,6 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('profile/<str:username>/',views.UserProfileView.as_view(), name='profile'),
-    path('sign-up/', views.UserCreateView.as_view(), name='signup'),
     path('login/', views.UserLoginView.as_view(), name='login'),
     path('logout/', views.UserLogoutView.as_view(), name='logout'),
     path('update/<str:username>/', views.UserUpdateView.as_view(), name='update'),
