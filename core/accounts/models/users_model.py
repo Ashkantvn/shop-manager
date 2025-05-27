d8b63650373cce54dea0338a66361ed0858868cf
Ashkan <ashkantavangar7@gmail.com>
2025-05-27 12:09:52 +0330

    Create models of manager, worker, customuser and workingtime

--- core/accounts/models/users_model.py
+++ core/accounts/models/users_model.py
@@ -2,4 +2,10 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    pass
+    first_name = models.CharField(max_length=30, blank=False, null=False)
+    last_name = models.CharField(max_length=30, blank=False, null=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
--- core/accounts/models/workers_model.py
+++ core/accounts/models/workers_model.py
@@ -2,4 +2,20 @@ from django.db import models
 
 
 class BusinessWorker(models.Model):
-    pass
+    user = models.OneToOneField(
+        'accounts.CustomUser', 
+        on_delete=models.CASCADE,
+        related_name='business_workers')
+    business_manager = models.ForeignKey(
+        'accounts.BusinessManager', 
+        on_delete=models.CASCADE,
+        related_name='workers')
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        verbose_name = 'Business Worker'
+        verbose_name_plural = 'Business Workers'
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
