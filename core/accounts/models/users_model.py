5ae3d8f8bd72e8243b786e74a6280f94a93def90
Ashkan <ashkantavangar7@gmail.com>
2025-05-29 18:37:15 +0330

    Indexing username

--- core/accounts/models/users_model.py
+++ core/accounts/models/users_model.py
@@ -2,10 +2,11 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
+    username = models.CharField(max_length=150, unique=True, blank=False, null=False,db_index=True)
     first_name = models.CharField(max_length=30, blank=False, null=False)
     last_name = models.CharField(max_length=30, blank=False, null=False)
     created_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_now=True)
+    updated_date = models.DateTimeField(auto_now=True)        
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
