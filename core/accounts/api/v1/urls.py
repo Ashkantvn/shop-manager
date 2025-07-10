0705ce27136283ba7cbb67fa2a2014f64318f6e4
Ashkan <ashkantavangar7@gmail.com>
2025-07-10 12:34:28 +0330

    Update urls of account api

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('profile/',views.UserProfileView.as_view(), name='profile'),
     path('login/', TokenObtainPairView.as_view(), name='login'),
     path('logout/', views.UserLogoutView.as_view(), name='logout'),
-    path("token/refresh", TokenRefreshView.as_view(), name="refresh_token"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="refresh_token"),
     path('update/<str:username>/', views.UserUpdateView.as_view(), name='update'),
 ]
