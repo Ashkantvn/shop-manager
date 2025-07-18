971d9dd4d027dd3a3275244341b871a9cb40e3e4
Ashkan <ashkantavangar7@gmail.com>
2025-07-18 11:46:25 +0330

    Update model of access token blacklist

--- core/accounts/models/access_token_blacklist_model.py
+++ core/accounts/models/access_token_blacklist_model.py
@@ -4,6 +4,8 @@ from django.db import models
 
 class AccessTokenBlackList(models.Model):
     token = models.CharField(max_length=255)
+    created_date = models.DateTimeField(auto_now=False,auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True,auto_now_add=False)
 
     def __str__(self):
         return f"Token: {self.token}"
