c56a0973fe3f1e04455f781353d86f2d1529db8d
Ashkan <ashkantavangar7@gmail.com>
2025-07-29 13:53:26 +0330

    Create task for delete expired token

--- core/accounts/tasks/delete_expired_token_task.py
+++ core/accounts/tasks/delete_expired_token_task.py
@@ -1,5 +1,15 @@
 import celery
+from accounts.models import AccessTokenBlackList
+from django.utils import timezone
+from datetime import timedelta
 
 @celery.shared_task
 def delete_expired_token():
-    pass
+    """
+    Task to delete expired access tokens from the blacklist.
+    """
+    cutoff = timezone.now() - timedelta(minutes=5)
+    for token in AccessTokenBlackList.objects.all():
+        if token.created_date < cutoff:
+            token.delete()
+    return "Expired tokens deleted successfully"
