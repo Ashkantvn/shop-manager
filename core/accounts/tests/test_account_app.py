a536de4c7f7a5a0ebc5588fbf1d3a07f403f6199
Ashkan <ashkantavangar7@gmail.com>
2025-08-10 11:31:45 +0330

    Update test and create initial template and view for app profile

--- core/accounts/tests/test_account_app.py
+++ core/accounts/tests/test_account_app.py
@@ -1,11 +1,12 @@
 import pytest
 from accounts.tests.fixtures import authenticated_manager
-
+from django.urls import reverse
 
 @pytest.mark.django_db
 class TestAccountApp:
     def test_app_profile_view(self, authenticated_manager):
         client = authenticated_manager
+        url = reverse("app-accounts:app-profile")
         response = client.get('/accounts/profile/')
         assert response.status_code == 200
         assert 'accounts/profile.html' in response.template_name
