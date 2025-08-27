6df0285df56646fc009314eb8801ac097b16c0ec
Ashkan <ashkantavangar7@gmail.com>
2025-08-27 18:22:46 +0330

    Make profile app view retrieve data and update template to show the profile data

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,6 +1,28 @@
-from django.views.generic import TemplateView
+from django.views import View
+from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from accounts.models import BusinessWorker
+
+User = get_user_model()
 
 # Create your views here.
-class AppProfileView(TemplateView):
-    template_name = 'accounts/profile.html'
+class AppProfileView(View):
+
+    def get(self, request):
+
+        user = request.user
+        profile = user
+
+        if hasattr(user , "business_manager"):
+            business_manager = user.business_manager
+            business_manager_workers = user.business_manager.workers.all()
+            profile = {
+                "business_manager": business_manager,
+                "business_manager_workers": business_manager_workers
+            }
+        elif hasattr(user, "business_workers"):
+            profile = user.business_workers
+
+
+        return render(request, 'accounts/profile.html',context={'profile':profile})
     
