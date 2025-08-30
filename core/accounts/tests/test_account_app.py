19a0d9437ba68e73b55291f7f229948ed02aa136
Ashkan <ashkantavangar7@gmail.com>
2025-08-30 23:49:45 +0330

    Create tests for login page(django Template version)

--- core/accounts/tests/test_account_app.py
+++ core/accounts/tests/test_account_app.py
@@ -1,13 +1,29 @@
 import pytest
 from accounts.tests.fixtures import authenticated_manager
 from django.urls import reverse
-from django.test import TestCase
+from django.test import Client
+
 
 @pytest.mark.django_db
 class TestAccountApp:
+    # Test for app profile view
     def test_app_profile_view(self, authenticated_manager):
+        """
+        Tests that the app profile view returns a 200 response with the correct template.
+        """
         client = authenticated_manager
         url = reverse("app-accounts:app-profile")
         response = client.get(url)
         assert response.status_code == 200
         assert 'accounts/profile.html' in [t.name for t in response.templates]
+
+    # Test for app login view
+    def test_app_login_view(self):
+        """
+        Tests that the app login view returns a 200 response with the correct template.
+        """
+        client = Client()
+        url = reverse("app-accounts:login")
+        response = client.get(url)
+        assert response.status_code == 200
+        assert 'accounts/login.html' in [t.name for t in response.templates]
