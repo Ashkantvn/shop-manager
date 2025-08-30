2a72f90b9a510b53cea3dc0ed966cef7a57f6493
Ashkan <ashkantavangar7@gmail.com>
2025-08-30 23:15:27 +0330

    Update tests to make views use slug instead of usename

--- core/accounts/tests/test_account_api.py
+++ core/accounts/tests/test_account_api.py
@@ -11,7 +11,7 @@ class TestAccountApi:
     # Test update account api
     # Manager can set the working time of workers each days
     def test_POST_update_account_status_201(self,worker , authenticated_manager):
-        url = reverse("accounts:update", args=[worker.user.username])
+        url = reverse("accounts:update", args=[worker.user.user_slug])
         data = {
             "start_time": "09:00",
             "end_time": "17:00",
@@ -20,7 +20,7 @@ class TestAccountApi:
         assert response.status_code == status.HTTP_201_CREATED
 
     def test_POST_update_account_status_400(self,worker , authenticated_manager):
-        url = reverse("accounts:update", args=[worker.user.username])
+        url = reverse("accounts:update", args=[worker.user.user_slug])
         data = {
             "start_time": "invalid_time",
             "end_time": "17:00",
@@ -31,7 +31,7 @@ class TestAccountApi:
 
     def test_POST_update_account_status_401(self,worker):
         client = APIClient()
-        url = reverse("accounts:update", args=[worker.user.username])
+        url = reverse("accounts:update", args=[worker.user.user_slug])
         data = {
             "start_time": "09:00",
             "end_time": "17:00",
@@ -40,7 +40,7 @@ class TestAccountApi:
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
     def test_POST_update_account_status_403(self,worker , authenticated_worker):
-        url = reverse("accounts:update", args=[worker.user.username])
+        url = reverse("accounts:update", args=[worker.user.user_slug])
         data = {
             "start_time": "09:00",
             "end_time": "17:00",
@@ -121,7 +121,7 @@ class TestAccountApi:
         client = APIClient()
         url = reverse("accounts:refresh_token")
         data = {
-            "refresh_token": "invalid_token"
+            "refresh": "invalid_token"
         }
         response = client.post(url,data=data, format='json')
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
