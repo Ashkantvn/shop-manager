27160f4be994fb7641e88dabbc53e594ae76595c
Ashkan <ashkantavangar7@gmail.com>
2025-08-31 00:06:49 +0330

    Create test for post method of login view (django template version)

--- core/accounts/tests/test_account_app.py
+++ core/accounts/tests/test_account_app.py
@@ -1,5 +1,5 @@
 import pytest
-from accounts.tests.fixtures import authenticated_manager
+from accounts.tests.fixtures import authenticated_manager, custom_user
 from django.urls import reverse
 from django.test import Client
 
@@ -7,7 +7,7 @@ from django.test import Client
 @pytest.mark.django_db
 class TestAccountApp:
     # Test for app profile view
-    def test_app_profile_view(self, authenticated_manager):
+    def test_app_GET_profile_view(self, authenticated_manager):
         """
         Tests that the app profile view returns a 200 response with the correct template.
         """
@@ -18,7 +18,7 @@ class TestAccountApp:
         assert 'accounts/profile.html' in [t.name for t in response.templates]
 
     # Test for app login view
-    def test_app_login_view(self):
+    def test_app_GET_login_view(self):
         """
         Tests that the app login view returns a 200 response with the correct template.
         """
@@ -27,3 +27,18 @@ class TestAccountApp:
         response = client.get(url)
         assert response.status_code == 200
         assert 'accounts/login.html' in [t.name for t in response.templates]
+
+    def test_app_POST_login_view(self, custom_user):
+        """
+        Tests that the app login view redirects to the profile page after successful login.
+        """
+        client = Client()
+        url = reverse("app-accounts:login")
+        
+        # Simulate a login
+        response = client.post(url, {'username': custom_user.username, 'password': 'testpassword'})
+        
+        # Check if the user is redirected to the profile page
+        assert response.status_code == 302
+        assert response.url == reverse("app-accounts:profile")
+
