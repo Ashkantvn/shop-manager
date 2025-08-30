f8800fd0b99a7a3c9aba926e4c48cd188bad7b9e
Ashkan <ashkantavangar7@gmail.com>
2025-08-30 23:16:23 +0330

    Make app use slug instead of username in update endpoint

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from accounts import models
+from django.contrib.auth.admin import UserAdmin
+
+
+class CustomUserAdmin(UserAdmin):
+    model = models.CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('user_slug',)}),
+    )
+    readonly_fields = ('user_slug','created_date','updated_date')
+    list_display = ('username', 'first_name', 'last_name', 'user_slug', 'created_date','updated_date')
+
+
+admin.site.register(models.CustomUser, CustomUserAdmin)
+
 # Register your models here.
-admin.site.register([models.AccessTokenBlackList,models.CustomUser, models.BusinessManager, models.BusinessWorker, models.WorkingTime])
+admin.site.register([models.AccessTokenBlackList, models.BusinessManager, models.BusinessWorker, models.WorkingTime])
 
 
 
