63c3472b573ee149665772d773be7e63d8165b23
Ashkan <ashkantavangar7@gmail.com>
2025-09-08 20:40:22 +0330

    Create view for product list api

--- core/products/api/v1/views.py
+++ core/products/api/v1/views.py
@@ -1,6 +1,12 @@
-from django.views import View
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+from products.models import Product
+from products.api.v1.serializers import ProductListSerializer
 
-
-
-class ProductListAPI(View):
-    pass
+class ProductListAPI(APIView):
+    
+    def get(self,request):
+        products_list = Product.objects.all()
+        serializer = ProductListSerializer(products_list,many=True)
+        return Response(data=serializer.data)
--- core/products/models/category.py
+++ core/products/models/category.py
@@ -7,5 +7,9 @@ class Category(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
     def __str__(self):
         return self.category_name
