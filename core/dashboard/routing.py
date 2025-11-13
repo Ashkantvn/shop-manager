3e4bbe31a4a4eef6c3a11903cecdc3ae4b86dccc
ashkan <ashkantavangar7@gmail.com>
2025-11-14 00:02:23 +0330

    Delete files

--- core/dashboard/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from dashboard import consumers
-
-
-websocket_urlpatterns = [
-    path("ws/products/", consumers.ProductConsumer.as_asgi()),
-]
--- core/static/js/accounts.js
+++
@@ -1,9 +0,0 @@
-// Profile codes
-const profile_grids = document.querySelectorAll(".profile-grid")
-
-
-profile_grids.forEach(profile_grid=>{
-        if (profile_grid.children.length === 1)
-            profile_grid.style.gridTemplateColumns= "1fr";
-    }
-)
