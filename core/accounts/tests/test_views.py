42d0d2245a62ea9a9734c4556ed78d1d8c7f36fc
Ashkan <ashkantavangar7@gmail.com>
2025-11-19 14:48:31 +0330

    Fixed test issues and create initial templates

--- core/accounts/tests/test_views.py
+++ core/accounts/tests/test_views.py
@@ -1,6 +1,7 @@
 import pytest
 from django.urls import reverse
 from django.test import Client
+from http import HTTPStatus as status
 
 @pytest.mark.django_db
 class TestAccountViews:
@@ -10,10 +11,10 @@ class TestAccountViews:
     def test_login_view_GET_200(self):
         url = reverse('accounts:login')
         response = self.client.get(url)
-        assert response.status_code == 200
+        assert response.status_code == status.OK
         assert 'accounts/login.html' in [template.name for template in response.templates]
 
-    def test_login_view_POST_302(self):
+    def test_login_view_POST_200(self, super_user):
         url = reverse('accounts:login')
         response = self.client.post(
             url,
@@ -22,8 +23,9 @@ class TestAccountViews:
                 'password': 'password'
             }
         )
-        assert response.status_code == 302
+        assert response.status_code == status.OK
         # Check user is logged in
+        response = self.client.get(url)
         is_authenticated = response.wsgi_request.user.is_authenticated
         assert is_authenticated, "User should be authenticated after login"
 
@@ -31,7 +33,7 @@ class TestAccountViews:
     def test_logout_view_POST_302(self, authenticated_client):
         url = reverse('accounts:logout')
         response = authenticated_client.post(url)
-        assert response.status_code == 302
+        assert response.status_code == status.FOUND
         # Check user is logged out
         is_authenticated = response.wsgi_request.user.is_authenticated
         assert not is_authenticated, "User should be logged out after logout"
@@ -39,4 +41,4 @@ class TestAccountViews:
     def test_logout_view_POST_not_authenticated_302(self):
         url = reverse('accounts:logout')
         response = self.client.post(url)
-        assert response.status_code == 302
+        assert response.status_code == status.FOUND
